a4bf57599da9a76380ae640b4d3f2e94d9df9f66
antoinem01 <antoinem@dds.nl>
2026-07-16 22:40:53 +0200

    Add GPLv3 copyright notice to sketch header

--- Torqeedo_motor.ino
+++ Torqeedo_motor.ino
@@ -1,3 +1,18 @@
+// Copyright (C) 2026 antoinem01
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
 // Torqeedo RS485 bus protocol (framing bytes 0xAC/0xAD/0xAE, CRC8) is based on
 // ArduPilot's AP_Torqeedo driver: https://github.com/ArduPilot/ardupilot
 // (libraries/AP_Torqeedo), licensed under the GNU GPLv3. See LICENSE.
